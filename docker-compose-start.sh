2d23c16c3fe1455aac8541bda3e366a3b0789e2a
vagrant <vagrant@vagrant.vm>
2020-11-12 12:43:20 +0000

    reorganize folders and environments for docker-compose-start.sh

--- docker-compose-start.sh
+++ docker-compose-start.sh
@@ -1,56 +1,38 @@
 #!/bin/bash
-curpwd=$PWD
-if [[ "$2" = "" ]]; then
-    echo "please specify platform local/production"
+if [[ "$1" = "up" ]]; then
+    action="up -d"
+elif [[ "$1" = "down" ]]; then
+    action=$1
+else
+    echo "specify either <up> or <down> actions"
     exit 1
 fi
+if [[ "$2" = "local" || "$2" = "production" ]]; then
+    echo "environment: $2" 
+else
+    echo "specify either <local> or <production> environments"
+    exit 1
+fi
+environment=$2
+homedir=$PWD
+services=(questinnovations paveltrujillo mydeskweb quenchinnovations)
+
 if [ "$1" = "up" ]; then
     docker-compose -f nginx/docker-compose.yml up -d
-    docker-compose -f paveltrujillo.info/docker-compose.yml up -d
-    cd mydeskweb
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file mydeskweb.com.env up -d
-    else
-        docker-compose --env-file mydeskweb.local.env up -d
-    fi
-    cd $curpwd
-    cd questinnovations
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file mydeskweb.com.env up -d
-    else
-        docker-compose --env-file mydeskweb.local.env up -d
-    fi
-    cd $curpwd
-    cd quenchinnovations
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file quenchinnovations.net.env up -d
-    else
-        docker-compose --env-file quenchinnovations.local.env -d
-    fi
-    cd $curpwd
+
+    for service in ${services[@]}; do
+        echo $service
+        cd $homedir/$service
+        docker-compose --env-file env.$environment up -d
+    done
+    cd $homedir
 else
-    docker-compose -f paveltrujillo.info/docker-compose.yml down
-    docker-compose -f questinnovations/docker-compose.yml down
-    cd mydeskweb
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file mydeskweb.com.env down
-    else
-        docker-compose --env-file mydeskweb.local.env down
-    fi
-    cd $curpwd
-    cd questinnovations
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file mydeskweb.com.env down
-    else
-        docker-compose --env-file mydeskweb.local.env down
-    fi
-    cd $curpwd
-    cd quenchinnovations
-    if [[ "$2" = "production" ]]; then
-        docker-compose --env-file quenchinnovations.net.env down
-    else
-        docker-compose --env-file quenchinnovations.local.env down
-    fi
-    cd $curpwd
+    for service in ${services[@]}; do
+        echo $service
+        cd $homedir/$service
+        docker-compose --env-file env.$environment down
+    done
+
+    cd $homedir
     docker-compose -f nginx/docker-compose.yml down
 fi
