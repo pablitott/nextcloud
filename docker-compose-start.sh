2dfdb1429f5778e20d6949ec72ac297460d4b25f
vagrant <vagrant@vagrant.vm>
2020-11-12 14:53:49 +0000

    "improve docker-compose-start.sh with loops, include project name in nginx, include

--- docker-compose-start.sh
+++ docker-compose-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#check arguments provided
 if [[ "$1" = "up" ]]; then
     action="up -d"
 elif [[ "$1" = "down" ]]; then
@@ -14,25 +15,37 @@ else
     exit 1
 fi
 environment=$2
-homedir=$PWD
-services=(questinnovations paveltrujillo mydeskweb quenchinnovations)
+echo "action: $action $environment"
 
-if [ "$1" = "up" ]; then
-    docker-compose -f nginx/docker-compose.yml up -d
-
-    for service in ${services[@]}; do
-        echo $service
-        cd $homedir/$service
-        docker-compose --env-file env.$environment up -d
-    done
-    cd $homedir
+#check core service status
+coreName=$(docker ps --filter name="nginx-proxy" --format {{.Names}})
+echo "coreName: $coreName"
+if [[ ! -z $coreName ]]; then
+    nginxStatus=$(docker inspect nginx-proxy -f {{.State.Status}})
+    echo "service: $coreName is $nginxStatus"
+    letsencryptStatus=$(docker inspect letsencrypt-proxy-companion -f {{.State.Status}})
+    NEXTCLOUD_NETWORK=$(docker inspect nginx-proxy -f {{.HostConfig.NetworkMode}})
 else
-    for service in ${services[@]}; do
-        echo $service
-        cd $homedir/$service
-        docker-compose --env-file env.$environment down
-    done
-
-    cd $homedir
-    docker-compose -f nginx/docker-compose.yml down
+    echo "core service is down"
 fi
+
+homedir=$PWD
+if [ "$1" = "up" ]; then
+    # for up services nginx must be the first
+    services=(nginx questinnovations paveltrujillo mydeskweb quenchinnovations)
+elif [ "$1" = "down" ]; then
+    # for down services nginx must be the last
+    services=(questinnovations paveltrujillo mydeskweb quenchinnovations nginx)
+fi
+for service in ${services[@]}; do
+    echo "turn $1 service $service"
+    cd $homedir/$service
+    if [[ -f env.$environment ]]; then
+        docker-compose --env-file env.$environment $action
+    else
+        docker-compose $action
+    fi
+done
+cd $homedir
+#print summary
+docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Size}}\t{{.Image}}"
