8b012dcf1025ba7645405bef73c6e79619646793
Pablo Trujillo <pablitott@gmail.com>
2021-04-24 08:16:26 -0400

    adding config.php for the 2 nextcloud servers, note the overwirte protocol line 32 to avoid network error in password change

---
+++ mydeskweb/config.php
@@ -0,0 +1,55 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocrywqatj2xl',
+  'passwordsalt' => 'H8TasS5/SejHH3zY17JVbPH6mCmYJd',
+  'secret' => 'Cn0Dfur7bORIMIOd7JcoRc6hW+KA2xhZz6PpdxqpeUNGYgf8',
+  'trusted_domains' => 
+  array (
+    0 => 'mydeskweb.com',
+    1 => 'www.mydeskweb.com'
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '20.0.1.1',
+  'overwrite.cli.url' => 'http://www.mydeskweb.com',
+  'overwriteprotocol' => 'https',
+  'dbname' => 'mydeskweb',
+  'dbhost' => 'mydeskweb.db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextclouduser',
+  'dbpassword' => 'MariaMagdalena',
+  'installed' => true,
+  'mail_from_address' => 'pablo.trujillo.tapia',
+  'mail_smtphost' => 'smtp.office365.com',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpauth' => 1,
+  'mail_smtpname' => 'pablo.trujillo.tapia@hotmail.com',
+  'mail_smtppassword' => 'Ca11ista',
+  'mail_smtpsecure' => 'tls',
+  'mail_domain' => 'hotmail.com',
+  'mail_smtpmode' => 'smtp',
+  'mail_sendmailmode' => 'smtp',
+  'mail_smtpport' => '587',
+  'maintenance' => false,
+  'theme' => '',
+  'loglevel' => 2,
+);
---
+++ quenchinnovations/config.php
@@ -0,0 +1,86 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocovmzo9yy6b',
+  'passwordsalt' => 'xXYssu4Hnzjhgya3Bk4xzSDD/7Pxez',
+  'secret' => 'm28XuyvLRAmAITfiSnUeScVkqWrdHlMN7J4ftU4+dpcpWVS7',
+  'trusted_domains' => 
+  array (
+    0 => 'quenchinnovations.net',
+    1 => 'www.quenchinnovations.net',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'app.mail.transport' => 'php.mail',
+  'dbtype' => 'mysql',
+  'version' => '20.0.1.1',
+  'overwrite.cli.url' => 'http://www.quenchinnovations.net',
+  'overwriteprotocol' => 'https',
+  'dbname' => 'quenchinnovations',
+  'dbhost' => 'quenchinnovations.db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextclouduser',
+  'dbpassword' => 'MariaMagdalena',
+  'installed' => true,
+  'trashbin_retention_obligation' => 'auto, 10',
+  'mail_smtpmode' => 'smtp',
+  'mail_sendmailmode' => 'smtp',
+  'mail_domain' => 'hotmail.com',
+  'mail_smtpport' => '587',
+  'mail_smtpdebug' => true,
+  'maintenance' => false,
+  'twofactor_enforced' => 'false',
+  'twofactor_enforced_groups' => 
+  array (
+  ),
+  'twofactor_enforced_excluded_groups' => 
+  array (
+  ),
+  'allowNightlyUpdates' => 
+  array (
+    0 => 'passwords',
+  ),
+  'mysql.utf8mb4' => true,
+  'loglevel' => 0,
+  'app_install_overwrite' => 
+  array (
+    0 => 'admin_notifications',
+  ),
+  'mail_from_address' => 'pablo.trujillo.tapia',
+  'enabledPreviewProviders' => 
+  array (
+    0 => 'OC\\Preview\\PNG',
+    1 => 'OC\\Preview\\JPEG',
+    2 => 'OC\\Preview\\GIF',
+    3 => 'OC\\Preview\\HEIC',
+    4 => 'OC\\Preview\\BMP',
+    5 => 'OC\\Preview\\XBitmap',
+    6 => 'OC\\Preview\\MP3',
+    7 => 'OC\\Preview\\TXT',
+    8 => 'OC\\Preview\\MarkDown',
+  ),
+  'mail_smtphost' => 'smtp.office365.com',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpauth' => 1,
+  'mail_smtpname' => 'pablo.trujillo.tapia@hotmail.com',
+  'mail_smtppassword' => 'Ca11ista',
+  'mail_smtpsecure' => 'tls',
+  'updater.release.channel' => 'stable',
+  'updater.secret' => '$2y$10$uoDj.qfXENRczKUEL4tH7eepsCH5AdeQzaMFkeR/RFFGSzR0RXOrC',
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+);
