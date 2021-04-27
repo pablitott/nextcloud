8b3f9245d4532ba43c2cae1a45fb159730f86298
Pablo Trujillo <pablitott@gmail.com>
2021-04-27 09:03:13 -0400

    fix occCmd.sh command

--- occCmd.sh
+++ occCmd.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 #####################################################################################
 #
@@ -8,15 +7,19 @@
 #   <ServiceName>    quenchinnovations.net or mydeskweb.com
 #   <occCmd> for a full list see https://docs.nextcloud.com/server/stable/admin_manual/configuration_server/occ_command.html#maintenance-commands-label
 #####################################################################################
+function occCmd()
+{
+    _color_red_="\e[31m"
+    _color_reset_="\e[0m"
 
-_color_red_="\e[31m"
-_color_reset_="\e[0m"
+    if [ -z $1 ] ; then
+        message="Service name is not provided, Syntax: occCmd <ServiceName> \n Where service Name would either QuestInnovations.net or mydeskweb.com"
+        echo $message
+        exit -1
+    fi
+    echo $*
+    ServiceName=$1
+    docker exec --user www-data $ServiceName php occ $2 $3 $4
 
-if [ -z $1 ] ; then
-    message="Service name is not provided, Syntax: occCmd <ServiceName> \n Where service Name would either QuestInnovations.net or mydeskweb.com"
-    echo -e "$(date +"%Y-%m-%d %T") $_color_red_ $message $_color_reset_"
-    exit -1
-fi
-echo $*
-ServiceName=$1
-docker exec -it --user www-data $ServiceName php occ $2 $3 $4
+}
+
