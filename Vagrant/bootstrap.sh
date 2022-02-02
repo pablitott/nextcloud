5d0670d4507b5ac483011f5a9867126abdd7ce0f
Pablo Trujillo <pablitott@gmail.com>
2022-02-02 10:02:17 -0500

    Install aws client on vm

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -4,3 +4,8 @@ apt-get upgrade -y
 # Basic Linux stuff
 apt-get install -y git
 apt-get install zip -y
+
+# AWS Cli tools
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
